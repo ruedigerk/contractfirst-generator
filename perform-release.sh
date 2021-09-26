753e77dcb8ae931eca6c086a8f1d06b3d118a7fb
Rüdiger Keller <ruedigerkeller@mail.de>
2021-09-26 10:10:59 +0200

    Added release shell script.

---
+++ perform-release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z "$1" ] || [ -z "$2" ] 
+then
+  echo "Usage: $0 <RELEASE_VERSION> <DEVELOPMENT_VERSION>"
+  exit 1
+fi
+
+RELEASE_VERSION="$1"
+DEVELOPMENT_VERSION="$2"
+
+export JAVA_HOME="C:\Program Files\AdoptOpenJDK\jdk-11.0.11.9-hotspot"
+
+mvn release:clean release:prepare release:perform -B -DreleaseVersion="$RELEASE_VERSION" -DdevelopmentVersion="$DEVELOPMENT_VERSION"
